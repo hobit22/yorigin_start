a99f46ebb6dd2f73714df35ee447c4a0677d6af0
hbkim <hobeen.kim@vitruv.co.kr>
2024-04-16 09:18:48 +0900

    [ADD] shop collection 단위 테스트

---
+++ app/tests/entities/collections/shop/test_shop_collection.py
@@ -0,0 +1,32 @@
+import pytest
+
+from app.entities.category.category_codes import CategoryCode
+from app.entities.collections.geo_json import GeoJsonPolygon
+from app.entities.collections.shop.shop_collection import ShopCollection
+from app.entities.collections.shop.shop_document import ShopDeliveryAreaSubDocument
+
+
+async def test_shop_insert_one() -> None:
+    # Given
+    name = "치킨집"
+    category_codes = [CategoryCode.CHICKEN]
+    delivery_areas = [
+        ShopDeliveryAreaSubDocument(
+            poly=GeoJsonPolygon(coordinates=[[[0, 0], [0, 10], [10, 10], [10, 0], [0, 0]]]),
+        )
+    ]
+
+    # When
+    shop = await ShopCollection.insert_one(name=name, category_codes=category_codes, delivery_areas=delivery_areas)
+    cursor = ShopCollection._collection.find({})
+    results = await cursor.to_list(100)
+
+    # Then
+    assert len(results) == 1
+    result = results[0]
+    assert result["_id"] == shop.id
+    assert result["name"] == shop.name
+    assert result["category_codes"] == ["chicken"]
+    assert result["delivery_areas"] == [
+        {"poly": {"type": "Polygon", "coordinates": [[[0, 0], [0, 10], [10, 10], [10, 0], [0, 0]]]}}
+    ]
