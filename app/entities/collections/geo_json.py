1028ad77b4c9a1a7e3cdeae519895ae8a2d49c8a
hbkim <hobeen.kim@vitruv.co.kr>
2024-04-17 05:56:08 +0900

    [ADD] 공간검색 point polygon intersect

--- app/entities/collections/geo_json.py
+++ app/entities/collections/geo_json.py
@@ -12,3 +12,17 @@ class GeoJsonPolygon:
 class GeoJsonPoint:
     coordinates: Sequence[float]
     type: Literal["Point"] = "Point"
+
+
+point = dataclasses.asdict(GeoJsonPoint(coordinates=[127.027667, 37.498563]))
+print(point)  # {'coordinates': [127.027667, 37.498563], 'type': 'Point'}
+
+print(
+    dataclasses.asdict(
+        GeoJsonPolygon(
+            coordinates=[[[0, 0], [0, 2], [2, 2], [2, 0], [0, 0]], [[1, 0], [1.5, 0.5], [1, 1], [0.5, 0.5], [1, 0]]]
+        )
+    )
+)
+
+# {'coordinates': [[[0, 0], [0, 2], [2, 2], [2, 0], [0, 0]], [[1, 0], [1.5, 0.5], [1, 1], [0.5, 0.5], [1, 0]]], 'type': 'Polygon'}
